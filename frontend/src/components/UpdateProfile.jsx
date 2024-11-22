4aca881b54c08cc28ba36e4cacb8737866213aec
Om Bhor <ombhor617@gmail.com>
2024-11-23 01:21:37 +0530

    submit handlers

--- frontend/src/components/UpdateProfile.jsx
+++ frontend/src/components/UpdateProfile.jsx
@@ -14,13 +14,24 @@ function UpdateProfile({ open, setOpen }) {
   const [input, setInput] = useState({
     fullname: user?.fullname,
     email: user?.email,
-    phoneNumber: user?.phoneNumbwe,
+    phoneNumber: user?.phoneNumber,
     bio: user?.profile?.bio,
     skills:user?.profile?.skills?.map((skill)=>skill),
     file:user?.profile?.resume
   });
 
-  
+  const changeEventHandler = (e)=>{
+    setInput({...input,[event.target.name]:e.target.value})
+  }
+
+  const fileChangeHandler = (e)=>{
+    const file = e.target.files?.[0]
+    setInput({...input , file})
+  }
+
+  const submitHandler = (e)=>{
+    e.preventDefault()
+  }
 
 
   return (
@@ -33,33 +44,35 @@ function UpdateProfile({ open, setOpen }) {
           <DialogHeader>
             <DialogTitle>Update Profile</DialogTitle>
           </DialogHeader>
-          <form>
+          <form onSubmit={submitHandler}>
             <div className="grid gap-4 py-4">
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="name">Name</Label>
-                <Input id="name" className="col-span-3" name="name" />
+                <Input type="text" id="name" onChange={changeEventHandler} value={input.fullname} className="col-span-3" name="name" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="email">email</Label>
-                <Input id="email" className="col-span-3" name="email" />
+                <Input type="email" id="email" className="col-span-3" onChange={changeEventHandler} value={input.email} name="email" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="number">Number</Label>
-                <Input id="number" className="col-span-3" name="number" />
+                <Input id="number" className="col-span-3" value={input.phoneNumber} onChange={changeEventHandler} name="number" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="bio">bio</Label>
-                <Input id="bio" className="col-span-3" name="bio" />
+                <Input id="bio" onChange={changeEventHandler} value={input.bio} className="col-span-3" name="bio" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="skills">skills</Label>
-                <Input id="skills" className="col-span-3" name="skills" />
+                <Input value={input.skills} id="skills" onChange={changeEventHandler} className="col-span-3" name="skills" />
               </div>
               <div className="grid grid-cols-4 items-center gap-4">
                 <Label htmlFor="file">resume</Label>
                 <Input
                   type="file"
                   id="file"
+                  onChange={fileChangeHandler}
+                  value={input.file}
                   accept="application/pdf"
                   className="col-span-3"
                   name="file"
